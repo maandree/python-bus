a6fc9f3ebc47e9b9aabd73f29275fc072914812a
Mattias Andrée <m@maandree.se>
2026-02-22 13:32:46 +0100

    Fix readme add makefile
    Signed-off-by: Mattias Andrée <m@maandree.se>

--- config.mk
+++ config.mk
@@ -3,12 +3,12 @@ MANPREFIX = $(PREFIX)/share/man
 PYTHONDIR = $(PREFIX)/lib/python$(PYTHON_MAJOR).$(PYTHON_MINOR)
 PYTHONPKGDIR = $(PYTHONDIR)/site-packages
 
-CC     = cc
+CC     = c99
 CYTHON = cython$(PYTHON_MAJOR)
 PYTHON = python$(PYTHON_VERSION)
 
-CPPFLAGS  = -D_DEFAULT_SOURCE -D_BSD_SOUCE -D_XOPEN_SOURCE=700
-CFLAGS    = -std=c99 -O2 $$(pkg-config --cflags python$(PYTHON_MAJOR))
+CPPFLAGS  = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700
+CFLAGS    = -O2 $$(pkg-config --cflags python$(PYTHON_MAJOR))
 LDFLAGS   = -s $$(pkg-config --libs python$(PYTHON_MAJOR)) -lbus
 
 PYTHON_MAJOR   = $$(python --version 2>&1 | cut -d ' ' -f 2 | cut -d . -f 1)
